294f12c94228d3b669d90daaec8437f1551a2977
martin-stoyanov <martin.a.stoyanov@gmail.com>
2020-04-30 09:56:06 -0400

    version 2 of app

--- utils/fetchData.ts
+++ utils/fetchData.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const serverURL = /*'https://travelbucket4300.herokuapp.com/';*/process.env.NODE_ENV === 'production' ? 'https://travelbucket4300.herokuapp.com/' : "http://0.0.0.0:5000";
+const serverURL = /*'https://travelbucket4300.herokuapp.com/';*/process.env.NODE_ENV === 'production' ? 'https://travelbucket4300-v2.herokuapp.com/' : "http://0.0.0.0:5000";
 
 export const fetchResults = async (searchCity: string, searchTopics: string) => {
     const result = await axios(
